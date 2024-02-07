f0d9c617098141ca7404dbf75242a4944513bf36
Ahmed Jamal <gongool@hotmail.com>
2024-02-07 23:18:26 +0200

    Last Update , all good

--- src/components/container/Footer.jsx
+++ src/components/container/Footer.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import ocqrcode from "../../assets/ocqrcode.png";
 import {
   BsFacebook,
   BsInstagram,
   BsTwitter,
   BsPinterest,
+  BsLinkedin, // Import LinkedIn icon
 } from "react-icons/bs";
 import { motion } from "framer-motion";
 
@@ -17,12 +18,13 @@ const Footer = () => {
     >
       <div className="grid md:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 place-items-start gap-8 text-white">
         <div>
-          <div className="font-bold mb-6">Get Started</div>
-          <p className="text-sm leading-7">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo neque
-            saepe cumque. Veritatis sunt commodi
-          </p>
+          <div className="font-bold mb-6">Location</div>
+          <div className="flex flex-col items-start gap-4">
+            <img src={ocqrcode} alt="Location QR Code" className="w-24" />
+            <p className="text-sm">41, Dokki Street, Office 803</p>
+          </div>
         </div>
+
         <div>
           <div className="font-bold mb-6">Services</div>
           <div className="flex flex-col gap-4">
@@ -33,7 +35,6 @@ const Footer = () => {
               Web Development
             </a>
             <a href="" className="text-sm hover:underline">
-              {" "}
               Science
             </a>
             <a href="" className="text-sm hover:underline">
@@ -61,9 +62,9 @@ const Footer = () => {
         <div>
           <div className="font-bold mb-6">Follow us</div>
           <div className="text-sm mb-4">skillexlearning@gmail.com</div>
-          <div className="text-sm">+959883271929</div>
-          <div className="text-sm">+959883271929</div>
-          <div className="text-sm">+959883271929</div>
+          <div className="text-sm">+20 1117776813</div>
+          <div className="text-sm">+20 1099966116</div>
+          <div className="text-sm">+20 1111106448</div>
           <div className="flex gap-4 mt-4">
             <a href="" className="hover:scale-110 text-xl">
               <BsFacebook />
@@ -77,8 +78,14 @@ const Footer = () => {
             <a href="" className="hover:scale-110 text-xl">
               <BsPinterest />
             </a>
+            <a href="" className="hover:scale-110 text-xl">
+              <BsLinkedin /> 
+            </a>
           </div>
         </div>
+      </div>
+      <div className="text-center mt-12 text-sm font-base text-white">
+         All rights reserved &copy; {new Date().getFullYear()}
       </div>
     </motion.div>
   );
